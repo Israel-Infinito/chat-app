780beea10247ca25ee4db334216c252b5518eeaa
Israel-Infinito <israelalagbe214@gmail.com>
2024-01-15 07:00:35 +0200

    Chat application

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
+const server = require('http').createServer(app);
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.get('/login-page', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+app.get('/register-page', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'register.html'));
+});
+
+app.get('/chat-page', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'chat.html'));
+});
+
+server.listen(port, () => {
+    console.log(`Server listening at port ${port}`);
 });
